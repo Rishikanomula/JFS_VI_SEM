355903fc537750babc30ed2627b7e30960e87b93
Rishika <rishikanomula2006@gmail.com>
2026-02-17 12:13:44 +0530

    hibernate project 1

---
+++ Eclipse_Projects/HibernateOneToMany/src/main/java/com/example/entity/Employee.java
@@ -0,0 +1,54 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "dept_id")
+	private Department department;
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public Employee(int id, String name, Department department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.department = department;
+	}
+	
+	
+}
